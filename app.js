53ed017742ab1044973d85ef1249dbb8dbe4f326
Felix Chen <fechen1993@gmail.com>
2022-03-07 18:10:21 -0500

    add event listeners to buttons

--- app.js
+++ app.js
@@ -1,19 +1,51 @@
-function add (x,y) {
+function add(x, y) {
     return x + y;
 }
 
-function subtract (x,y) {
+function subtract(x, y) {
     return x - y;
 }
 
-function multiply (x,y) {
+function multiply(x, y) {
     return x * y;
 }
 
-function divide (x,y) {
+function divide(x, y) {
     return x / y;
 }
 
-function operate (operator,x,y) {
-    return operator(x,y);
+function operate(operator, x, y) {
+    return operator(x, y);
 }
+
+const numberButtons = document.querySelectorAll('.number-button');
+const display = document.querySelector('#calc-display');
+
+for (let button of numberButtons) {
+    button.addEventListener('click', function () {
+        display.value += button.innerText;
+    })
+}
+
+const addButton = document.querySelector('.add-button');
+const subtractButton = document.querySelector('.subtract-button');
+const multiplyButton = document.querySelector('.multiply-button');
+const divideButton = document.querySelector('.divide-button');
+const equalButton = document.querySelector('.equal-button');
+let firstNumber;
+let secondNumber;
+let selectedOperator;
+
+addButton.addEventListener('click', function (e) {
+    firstNumber = +display.value;
+    display.value = "";
+    selectedOperator = addButton.innerText;
+})
+
+equalButton.addEventListener('click', function (e) {
+    secondNumber = +display.value;
+    if(selectedOperator === '+') {
+       let result = add(firstNumber, secondNumber);
+       display.value = result;
+    }
+})
